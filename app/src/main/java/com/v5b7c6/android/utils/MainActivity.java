5e77ac4da515df99770a8ca7aa403e940c1c3539
v5b7c6 <577750993@qq.com>
2020-01-08 00:46:13 +0800

    add init

---
+++ app/src/main/java/com/v5b7c6/android/utils/MainActivity.java
@@ -0,0 +1,13 @@
+package com.v5b7c6.android.utils;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
