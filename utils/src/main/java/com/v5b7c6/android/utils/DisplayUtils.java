b9965a6378e3cb3e9e4d76e550c7c9a85894ce57
v5b7c6 <577750993@qq.com>
2020-01-11 21:26:12 +0800

    完善DisplayUtils

--- utils/src/main/java/com/v5b7c6/android/utils/DisplayUtils.java
+++ utils/src/main/java/com/v5b7c6/android/utils/DisplayUtils.java
@@ -1,4 +1,37 @@
 package com.v5b7c6.android.utils;
 
 public class DisplayUtils {
+
+    /**
+     * 将px值转换为dip或dp值，保证尺寸大小不变
+     */
+    public static float px2dip(float pxValue) {
+        final float scale = Utils.getApp().getResources().getDisplayMetrics().density;
+        return (pxValue / scale);
+    }
+
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变，类似Context.getDimension方法
+     */
+    public static float dip2px(float dipValue) {
+        final float scale = Utils.getApp().getResources().getDisplayMetrics().density;
+        return (dipValue * scale);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     */
+    public static int px2sp(float pxValue) {
+        final float fontScale = Utils.getApp().getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     */
+    public static int sp2px(float spValue) {
+        final float fontScale = Utils.getApp().getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale);
+    }
+
 }
