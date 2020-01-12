c97d5cbd438bd2a24d7de3f0ddf4d8ace5a8dc7c
v5b7c6 <577750993@qq.com>
2020-01-12 16:36:12 +0800

    完善MainActivity

--- app/src/main/java/com/v5b7c6/android/utils/MainActivity.java
+++ app/src/main/java/com/v5b7c6/android/utils/MainActivity.java
@@ -1,13 +1,43 @@
 package com.v5b7c6.android.utils;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+
+        textView.setText(getInfo());
+    }
+
+    private String getInfo() {
+        return "AppName：" + AppUtils.getAppName() + "\n"
+                + "VersionName：" + AppUtils.getVersionName() + "\n"
+                + "VersionCode：" + AppUtils.getVersionCode() + "\n"
+                + "DeviceRoot：" + DeviceUtils.isDeviceRoot() + "\n"
+                + "AndroidID：" + DeviceUtils.getAndroidID() + "\n"
+                + "Manufacturer：" + DeviceUtils.getManufacturer() + "\n"
+                + "Model：" + DeviceUtils.getModel() + "\n"
+                + "SDKVersionName：" + DeviceUtils.getSDKVersionName() + "\n"
+                + "SDKVersion：" + DeviceUtils.getSDKVersion() + "\n"
+                + "NetworkConnected：" + NetworkUtils.isNetworkConnected() + "\n"
+                + "NetworkType：" + NetworkUtils.getNetworkType() + "\n"
+                + "ScreenWidth：" + ScreenUtils.getScreenWidth() + "\n"
+                + "ScreenHeight：" + ScreenUtils.getScreenHeight() + "\n"
+                + "AppScreenWidth：" + ScreenUtils.getAppScreenWidth() + "\n"
+                + "AppScreenHeight：" + ScreenUtils.getAppScreenHeight() + "\n"
+                + "ScreenDensity：" + ScreenUtils.getScreenDensity() + "\n"
+                + "ScreenDensityDpi：" + ScreenUtils.getScreenDensityDpi() + "\n"
+                + "checkPhone（15711095907）：" + RegexUtils.checkPhone("15711095907");
     }
 }
