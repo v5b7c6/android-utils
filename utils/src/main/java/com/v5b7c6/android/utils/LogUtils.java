687219ceb4876d7f9f401e999804a1ed3e22c14a
v5b7c6 <577750993@qq.com>
2020-01-13 21:30:45 +0800

    完善LogUtils

--- utils/src/main/java/com/v5b7c6/android/utils/LogUtils.java
+++ utils/src/main/java/com/v5b7c6/android/utils/LogUtils.java
@@ -33,7 +33,6 @@ public class LogUtils {
     private static boolean sLogSwitch       = true; // log总开关
     private static String  sGlobalTag       = null; // log标签
     private static boolean sTagIsSpace      = true; // log标签是否为空白
-    private static boolean sLog2FileSwitch  = false;// log写入文件开关
     private static boolean sLogBorderSwitch = true; // log边框开关
     private static int     sLogFilter       = V;    // log过滤器
 
